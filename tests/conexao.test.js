feed18db6b4da1b565c40c09f29cd747247eea5f
Sérgio Moura <smouracalmon@gmail.com>
2023-03-29 11:22:59 -0300

    configuração do sequelize

---
+++ tests/conexao.test.js
@@ -0,0 +1,28 @@
+// 1 - Importando o sequelize
+const sequelize = require('sequelize');
+
+// 2 - Importar configurações
+const config = require('../databases/config').development;
+
+// 3 - Criar a conexão com o banco de dados
+const conexao = new sequelize(config);
+
+// 4 executar uma consulta
+const sql = `SELECT
+                p.id,
+                p.nome,
+                ifnull(SUM(pp.quantidade), 0) as quantidade
+            FROM
+                pizzas as p
+                LEFT JOIN pedido_pizza as pp ON p.id = pp.pizza_id
+            group by p.id, p.nome;`
+
+const promessa = conexao.query(sql);
+
+promessa.then(
+    dados => {
+        console.log(dados);
+    }
+)
+
+
